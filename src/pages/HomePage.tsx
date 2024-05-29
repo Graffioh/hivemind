f0664d3230663c3ed7f6d3977cc5cb61d15884c6
Graffioh <bregliascuola2002@gmail.com>
2024-05-29 21:43:51 +0200

    Refactor HomePage code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
     },
   });
 
-  const handlePost = () => {
+  function handlePost() {
     if (textAreaRef.current && titleInputRef.current) {
       const title = titleInputRef.current.value;
       const content = textAreaRef.current.value;
@@ -58,7 +58,13 @@ export default function HomePage() {
       textAreaRef.current.value = "";
       setIsPostValid(false);
     }
-  };
+  }
+
+  function handleInputChange() {
+    const title = titleInputRef.current?.value || "";
+    const content = textAreaRef.current?.value || "";
+    setIsPostValid(title !== "" && content !== "");
+  }
 
   if (error) {
     return <span>Error: {error.message}</span>;
@@ -70,69 +76,92 @@ export default function HomePage() {
         <div className="text-3xl font-bold mt-4">Hivemind</div>
         <div className="flex flex-col w-full">
           <LoginSection />
-          <div className="flex flex-col items-center">
-            <input
-              ref={titleInputRef}
-              className="w-64 p-1 rounded border-x-2 border-t-2 border-neutral-600"
-              placeholder="Title"
-              required
-              onChange={() => {
-                const areInputsEmpty =
-                  titleInputRef.current?.value == "" ||
-                  textAreaRef.current?.value == "";
-                if (areInputsEmpty) {
-                  setIsPostValid(false);
-                } else {
-                  setIsPostValid(true);
-                }
-              }}
-            ></input>
-            <textarea
-              ref={textAreaRef}
-              rows={10}
-              cols={50}
-              className="p-1 rounded border-2 border-neutral-600"
-              placeholder="Write your thoughts..."
-              required
-              onChange={() => {
-                const areInputsEmpty =
-                  titleInputRef.current?.value == "" ||
-                  textAreaRef.current?.value == "";
-                if (areInputsEmpty) {
-                  setIsPostValid(false);
-                } else {
-                  setIsPostValid(true);
-                }
-              }}
-            ></textarea>
-            <button
-              onClick={handlePost}
-              className="m-4 w-24 h-12 disabled:bg-stone-800"
-              disabled={!isPostValid}
-            >
-              post
-            </button>
-          </div>
-          <div className="flex flex-col mx-20 rounded items-center mt-4">
-            <p className="font-bold text-white text-2xl">Thoughts Board</p>
-            {data ? (
-              data.pages.map((page) => (
-                <div
-                  key={page.currentPage}
-                  className="flex flex-col gap-2 w-full"
-                >
-                  {page.data.map((post) => (
-                    <PostSection key={post.id} post={post} />
-                  ))}
-                </div>
-              ))
-            ) : (
-              <div>...</div>
-            )}
-            <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
-          </div>
+          <PostForm
+            titleInputRef={titleInputRef}
+            textAreaRef={textAreaRef}
+            isPostValid={isPostValid}
+            handleInputChange={handleInputChange}
+            handlePost={handlePost}
+          />
+          <ThoughtsBoard
+            data={data}
+            ref={ref}
+            isFetchingNextPage={isFetchingNextPage}
+          />
         </div>
       </div>
     </>
+  );
+}
+
+interface PostFormProps {
+  titleInputRef: React.RefObject<HTMLInputElement>;
+  textAreaRef: React.RefObject<HTMLTextAreaElement>;
+  isPostValid: boolean;
+  handleInputChange: () => void;
+  handlePost: () => void;
+}
+
+function PostForm({
+  titleInputRef,
+  textAreaRef,
+  isPostValid,
+  handleInputChange,
+  handlePost,
+}: PostFormProps) {
+  return (
+    <div className="flex flex-col items-center">
+      <input
+        ref={titleInputRef}
+        className="w-64 p-1 rounded border-x-2 border-t-2 border-neutral-600"
+        placeholder="Title"
+        required
+        onChange={handleInputChange}
+      />
+      <textarea
+        ref={textAreaRef}
+        rows={10}
+        cols={50}
+        className="p-1 rounded border-2 border-neutral-600"
+        placeholder="Write your thoughts..."
+        required
+        onChange={handleInputChange}
+      />
+      <button
+        onClick={handlePost}
+        className="m-4 w-24 h-12 disabled:bg-stone-800 font-bold"
+        disabled={!isPostValid}
+      >
+        Post
+      </button>
+    </div>
+  );
+}
+
+interface ThoughtsBoardProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
+  ref: (node?: Element | null | undefined) => void;
+  isFetchingNextPage: boolean;
+}
+
+function ThoughtsBoard({ data, ref, isFetchingNextPage }: ThoughtsBoardProps) {
+  return (
+    <div className="flex flex-col mx-20 rounded items-center mt-4">
+      <p className="font-bold text-white text-2xl">Thoughts Board</p>
+      {data ? (
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        data.pages.map((page: any) => (
+          <div key={page.currentPage} className="flex flex-col gap-2 w-full">
+            {page.data.map((post: Post) => (
+              <PostSection key={post.id} post={post} />
+            ))}
+          </div>
+        ))
+      ) : (
+        <div>...</div>
+      )}
+      <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
+    </div>
   );
 }
