f147cb06f14d6d33fc4440faedd16d46d87de4c7
Graffioh <bregliascuola2002@gmail.com>
2024-06-17 09:52:51 +0200

    Refactor go code

--- server-hivemind/main.go
+++ server-hivemind/main.go
@@ -4,12 +4,8 @@ import (
 	"log"
 	"net/http"
 	"server-hivemind/config"
-	"server-hivemind/handlers"
-	"server-hivemind/repository"
+	"server-hivemind/router"
 	"time"
-
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
 func main() {
@@ -21,66 +17,12 @@ func main() {
 func run() error {
 	config.LoadEnv()
 
-	// DB CONFIGURATION
+	// DB setup
 	dbConnectionString := config.GetDBConnectionString()
 	config.InitDB(dbConnectionString)
-
 	db := config.GetDB()
 
-	// Router init
-	router := mux.NewRouter()
-
-	// AllowedOrigins: * only for dev env
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173"},
-		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodOptions, http.MethodPut, http.MethodDelete},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-	})
-
-	// HANDLERS
-	//
-	// Users
-	ur := repository.NewUserRepository(db)
-	uh := handlers.NewUsers(ur)
-	//
-	// Posts
-	pr := repository.NewPostRepository(db)
-	ph := handlers.NewPosts(pr)
-	//
-	// Comments
-	cr := repository.NewCommentRepository(db)
-	ch := handlers.NewComments(cr)
-	//
-	// Reactions
-	rr := repository.NewReactionRepository(db)
-	rh := handlers.NewReactions(rr)
-
-	// ROUTES
-	//
-	// Users
-	router.HandleFunc("/user/current", uh.GetCurrentUser).Methods("GET")
-	router.HandleFunc("/user/{id:[0-9]+}", uh.GetUserById).Methods("GET")
-	router.HandleFunc("/user", uh.CreateOrLoginUser).Methods("POST")
-	router.HandleFunc("/user/logout/{id:[0-9]+}", uh.DeleteSession).Methods("GET")
-	//
-	// Posts
-	router.HandleFunc("/post", ph.GetPosts).Methods("GET")
-	router.HandleFunc("/post/pagination", ph.GetPostsWithPagination).Methods("GET")
-	router.HandleFunc("/post/count", ph.GetTotalPostsCount).Methods("GET")
-	router.HandleFunc("/post/{id:[0-9]+}", ph.GetPost).Methods("GET")
-	router.HandleFunc("/post", ph.CreatePost).Methods("POST")
-	//
-	// Comments
-	router.HandleFunc("/comment/{post_id:[0-9]+}", ch.GetComments).Methods("GET")
-	router.HandleFunc("/comment/{post_id:[0-9]+}", ch.CreateComment).Methods("POST")
-	//
-	// Reactions
-	router.HandleFunc("/reaction/post/{post_id:[0-9]+}", rh.GetPostReactions).Methods("GET")
-	router.HandleFunc("/reaction/comment/{comment_id:[0-9]+}", rh.GetCommentReactions).Methods("GET")
-	router.HandleFunc("/reaction", rh.CreateReaction).Methods("POST")
-
-	server_handler := c.Handler(router)
+	server_handler := router.NewRouter(db)
 
 	log.Println("Starting server on :8080")
 
---
+++ server-hivemind/router/router.go
@@ -0,0 +1,55 @@
+// router/router.go
+
+package router
+
+import (
+	"database/sql"
+	"net/http"
+
+	"server-hivemind/handlers"
+	"server-hivemind/repository"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func NewRouter(db *sql.DB) http.Handler {
+
+	router := mux.NewRouter()
+
+	ur := repository.NewUserRepository(db)
+	uh := handlers.NewUsers(ur)
+	pr := repository.NewPostRepository(db)
+	ph := handlers.NewPosts(pr)
+	cr := repository.NewCommentRepository(db)
+	ch := handlers.NewComments(cr)
+	rr := repository.NewReactionRepository(db)
+	rh := handlers.NewReactions(rr)
+
+	router.HandleFunc("/user/current", uh.GetCurrentUser).Methods("GET")
+	router.HandleFunc("/user/{id:[0-9]+}", uh.GetUserById).Methods("GET")
+	router.HandleFunc("/user", uh.CreateOrLoginUser).Methods("POST")
+	router.HandleFunc("/user/logout/{id:[0-9]+}", uh.DeleteSession).Methods("GET")
+
+	router.HandleFunc("/post", ph.GetPosts).Methods("GET")
+	router.HandleFunc("/post/pagination", ph.GetPostsWithPagination).Methods("GET")
+	router.HandleFunc("/post/count", ph.GetTotalPostsCount).Methods("GET")
+	router.HandleFunc("/post/{id:[0-9]+}", ph.GetPost).Methods("GET")
+	router.HandleFunc("/post", ph.CreatePost).Methods("POST")
+
+	router.HandleFunc("/comment/{post_id:[0-9]+}", ch.GetComments).Methods("GET")
+	router.HandleFunc("/comment/{post_id:[0-9]+}", ch.CreateComment).Methods("POST")
+
+	router.HandleFunc("/reaction/post/{post_id:[0-9]+}", rh.GetPostReactions).Methods("GET")
+	router.HandleFunc("/reaction/comment/{comment_id:[0-9]+}", rh.GetCommentReactions).Methods("GET")
+	router.HandleFunc("/reaction", rh.CreateReaction).Methods("POST")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodOptions, http.MethodPut, http.MethodDelete},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	return c.Handler(router)
+}
